70d8d6923e55ab3d87b4be174635b4b7b4b566e2
anupampahari <anupam.pahari@gmail.com>
2023-07-23 17:26:38 +0530

    4 files modified

--- src/app/mathjax/mathjax.component.ts
+++ src/app/mathjax/mathjax.component.ts
@@ -1,57 +1,61 @@
-import { Component, OnInit,Input,OnChanges } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 import { SimpleChanges } from '@angular/core';
-import {GlobalService} from '../global.service';
+import { GlobalService } from '../global.service';
 // const MathJax = Window['mathjax'];
 @Component({
   selector: 'mathjax',
-  inputs:['content'],
+  inputs: ['content'],
   templateUrl: './mathjax.component.html',
-  styleUrls: ['./mathjax.component.css']
+  styleUrls: ['./mathjax.component.css'],
 })
 export class MathjaxComponent implements OnChanges {
-  @Input() content :string;
+  @Input() content: string;
 
-  constructor(public gs:GlobalService) { }
-  mathJaxObject; 
+  constructor(public gs: GlobalService) {}
+  mathJaxObject;
   ngOnChanges(changes: SimpleChanges) {
-   if (changes['content']) {
-    // console.log("content chnaged")
-      this.renderMath()
+    if (changes['content']) {
+      // console.log("content chnaged")
+      this.renderMath();
     }
   }
-  
-  renderMath(){
-  // console.log("render math")
-// MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
 
-  this.mathJaxObject  = this.gs.nativeGlobal()['MathJax'] ;
-  //setInterval(()=>{},1) 
-  let angObj = this;
-  setTimeout(()=>{
-    console.log("1234")
-  angObj.mathJaxObject.Hub.Queue(["Typeset",angObj.mathJaxObject.Hub],'mathContent');
+  renderMath() {
+    // console.log("render math")
+    // MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
 
-  },1000)
-  } 
-  loadMathConfig(){
-    console.log("load config")
+    this.mathJaxObject = this.gs.nativeGlobal()['MathJax'];
+    //setInterval(()=>{},1)
+    let angObj = this;
+    setTimeout(() => {
+      console.log('1234');
+      angObj.mathJaxObject.Hub.Queue(
+        ['Typeset', angObj.mathJaxObject.Hub],
+        'mathContent'
+      );
+    }, 1000);
+  }
+  loadMathConfig() {
+    console.log('load config');
 
-this.mathJaxObject  = this.gs.nativeGlobal()['MathJax'] ;
-    this.mathJaxObject.Hub.Config({        
+    this.mathJaxObject = this.gs.nativeGlobal()['MathJax'];
+    this.mathJaxObject.Hub.Config({
       showMathMenu: false,
-      tex2jax: {inlineMath: [["$","$"],["\\(","\\)"]]},
-      menuSettings: { zoom: "Double-Click",zscale: "150%" },
+      tex2jax: {
+        inlineMath: [
+          ['$', '$'],
+          ['\\(', '\\)'],
+        ],
+      },
+      menuSettings: { zoom: 'Double-Click', zscale: '150%' },
       CommonHTML: { linebreaks: { automatic: true } },
-      "HTML-CSS": { linebreaks: { automatic: true } },
-             SVG: { linebreaks: { automatic: true } }
+      'HTML-CSS': { linebreaks: { automatic: true } },
+      SVG: { linebreaks: { automatic: true } },
     });
-  } 
-
-  ngOnInit(){
-
-     this.loadMathConfig()
-     this.renderMath();
-
   }
 
+  ngOnInit() {
+    this.loadMathConfig();
+    this.renderMath();
+  }
 }
